b3fb9ad4d51087910d1718a9963c13ebfbcbabcf
anibaldel <anibaldel2669@gmail.com>
2020-10-06 22:27:00 -0400

    Backend terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,36 @@
 const { io } = require('../index');
-
+const { comprobarJWT } = require('../helpers/jwt');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje } = require('../controller/socket');
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
+
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+    // verificar Auntentificacion
+    if (!valido) {return client.disconnect();}
+
+    // Cliente autenticado
+    usuarioConectado(uid);
+
+    // Ingresar al usuario a una sala particular
+    // sala global, client.id, das4d6sd456d
+    client.join(uid);
+
+    // Escuchar de cliente el mensaje personal
+    client.on('mensaje-personal', async (payload)=> {
+
+        console.log(payload);
+
+        // await grabarMensaje( payload );
+
+        io.to(payload.para).emit('mensaje-personal', payload);
+    })
+
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     // client.on('mensaje', ( payload ) => {
